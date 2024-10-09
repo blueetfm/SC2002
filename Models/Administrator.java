1877989a137983f81e46b8ee1d4b0d90e0f7b11e
ZongZheJiang <jiangzongzhe374@gmail.com>
2024-10-09 19:14:17 +0800

    initialised more models

---
+++ Models/Administrator.java
@@ -0,0 +1,11 @@
+package Models;
+
+import Views.AdministratorMenu;
+
+public class Administrator implements AdministratorMenu{
+
+	public Administrator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Models/Pharmacist.java
@@ -0,0 +1,11 @@
+package Models;
+
+import Views.PharmacistMenu;
+
+public class Pharmacist implements PharmacistMenu{
+
+	public Pharmacist() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
